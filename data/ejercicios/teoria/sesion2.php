1687b94436130c9965b23f428ecdf25ac5a6bc7a
Sergio Respau <sergiorespau@gmail.com>
2022-10-24 17:10:15 +0200

    subida teoria

--- data/ejercicios/teoria/sesion2.php
+++ data/ejercicios/teoria/sesion2.php
@@ -2,6 +2,14 @@
 //Se une a la sesion ya comenzada por la página anterior. Buscara una cookie llamada PHPSESSID y se unira a ella
 session_start();
 
+//Combinación para borrar y destruir la información de la sesión. Se destruye pero no se borra la cookie establecida
+$_SESSION = array();
+session_destroy();
+//tambien hay que borrar la cookie
+setcookie(session_name(),"", time() -120, '/');//session_name para obtener el valor de la sesion. 
+//Ponemos '/' para chrome (y ya para todos) porque puede que no borre la cookie si no lo ponemos
+
+
 echo "La página se ha cagado " . $_SESSION["contador"] . " veces. <br>";
 
 echo "El producto en la lista de compra es: " . $_SESSION['producto'];
