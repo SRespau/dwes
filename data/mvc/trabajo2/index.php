90feb138cc0e34b5697a69d6e9d293e09537b8bb
SRespau <113921937+SRespau@users.noreply.github.com>
2022-11-07 16:21:32 +0100

    Delete data/mvc/trabajo2 directory

--- data/mvc/trabajo2/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-    // Inicio de la aplicación. Añadirá el codigo de app.php, creará un objeto con el constructor App y luego correra la función run();
-    require_once "App.php";
-    $app = new App;
-    $app->run();
-?>
--- data/mvc/trabajo2/vistas/header.php
+++
@@ -1,9 +0,0 @@
-<header>
-  <!-- Cabecera con 3 links, Indice, login y logout -->
-
-  <ul>
-    <li><a href="?method=index">Inicio</a></li>
-    <li><a href="?method=login">Login</a></li>
-    <li><a href="?method=logout">Logout</a></li>
-  </ul>
-</header>
